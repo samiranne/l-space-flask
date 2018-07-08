71ddca506a7b62872a1a5896b1c7e154d150168a
Samira Nedungadi <samira.nedungadi@gmail.com>
2018-07-07 18:44:15 -0700

    Upgrade to Flask 1.0

--- app_factory.py
+++ app_factory.py
@@ -1,8 +1,9 @@
 import os
+import logging
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.bcrypt import Bcrypt
-from flask.ext.login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 from config import DevelopmentConfig
 
 app = Flask(__name__)
