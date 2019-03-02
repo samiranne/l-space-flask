f345caf2179090d1f6d415f65b30490e052009f9
samiranne <samira.nedungadi@gmail.com>
2019-03-02 02:02:31 -0800

    Change site name to L-space

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import os
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', "postgresql://library:godrics@localhost/library_dev") #defaults to localhost
-    SECRET_KEY = "d97a21fb8e3d2b329b9278d1b66285af" # use the following in terminal to generate: "cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', "postgresql://library:godrics@localhost/library_dev")
+    SECRET_KEY = "d97a21fb8e3d2b329b9278d1b66285af"
     GOOGLE_BOOKS_API_KEY = "AIzaSyBYhBTKNfv4JAw_f1xSpBXJ5v2wXazr1sw"
 
 class SiteConfig:
     # these config values are used in the templates (e.g. open graph, twitter)
-    SITE_NAME = "Godric's Hollow Library"
-    SITE_KEYWORDS = "library, rationalist"
-    SITE_OWNER = "Chelsea Voss and Samira Nedungadi"
+    SITE_NAME = "L-space"
+    SITE_KEYWORDS = "library, books"
+    SITE_OWNER = "Samira Nedungadi"
 
     SITE_IMAGE = "img/book_icon.png"
     SITE_IMAGE_WIDTH = 600
@@ -29,6 +29,8 @@ class DevelopmentConfig(Config, SiteConfig):
 
 
 class TestConfig(Config):
+    # I would like to use 'sqlite:///:memory:' rather than having to create a test db, but ran into this issue:
+    # http://stackoverflow.com/q/11860804/145400
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', "postgresql://library:godrics@localhost/library_test")
     TESTING = True
     SQLALCHEMY_ECHO = False #prints out the created queries to the terminal
